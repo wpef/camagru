e8dba23362a5a706c767f78d7946b27cfb684bd7
fde-monc <fde-monc@student.42.fr>
2017-01-23 17:40:40 +0100

    ERROR CONNECTION ?? Surement PAMP probleme

---
+++ config/manage_db.php
@@ -0,0 +1,24 @@
+<?php
+
+function create_table($name, $values) {
+//create_table returns a string formated as SQL request creating a table of name $name and columns such as referenced in $values ('name' => 'type');
+//create_table was created to use withe PDO->exec() function;
+	
+	$create = "CREATE TABLE " . $name . " (";
+	$col = "";
+	$end = ");";
+	
+	if (!isset($values))
+		return ($create . $end);
+
+	$i = 0;
+	foreach ($values as $vname => $type)
+	{
+		$col = $col . $vname . " " . $type;
+		if ($i < count($values) - 1)
+			$col = $col . ', ';
+		$i++;
+	}
+	return $create . $col . $end;
+}
+?>
--- config/setup.php
+++ config/setup.php
@@ -1,8 +1,37 @@
 <?php
 
 require_once('database.php');
+require_once('manage_db.php');
 
-echo YOYO;
-$db = new PDO($DB_DSN, $DB_USER, $DB_PASSWORD);
-var_dump($db);
+$drop = "DROP DATABASE IF EXISTS " . $DB_NAME . ";";
+$create_db = "CREATE DATABASE " . $DB_NAME . ";";
+
+//Connect to MySql (db not created yet);
+try {
+	$db = new PDO('mysql:host=localhost', $DB_USER, $DB_PASSWORD);
+	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOexception $e) {
+	die('ERROR CREATING DB :' . $e->getMessage());
+}
+
+//Create DB
+$db->exec($drop);
+$db->exec($create_db) or die (print_r($db->errorInfo(), true));
+
+//Create tables;
+$user_a = array (
+		'ID'		=>	'varchar(30)',
+		'login' 	=>	'varchar(30)',
+		'pass'		=>	'varchar(30)',
+		'mail'		=>	'varchar(30)',
+		'f_name'	=>	'varchar(30)',
+		'l_name'	=>	'varchar(30)',
+		'pict_id'	=>	'varchar(30)'
+	);
+
+$db->exec("USE " . $DB_NAME);
+$db->exec(create_table('users', $user_a));
+
+// var_dump($db);
+
 ?>
