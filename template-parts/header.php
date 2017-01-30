971ef3ee4e4e7c5125420c15db9302464a35c5a4
wPef <f.demoncade@gmail.com>
2017-01-30 20:05:09 +0100

    try ROOT global var

--- template-parts/header.php
+++ template-parts/header.php
@@ -1,13 +1,13 @@
 <header>
-	<a href = "/">
-		<img src="/img/logo.png" id='logo'/>
+	<a href = "<?php echo _ROOT_;?>">
+		<img src = "<?php echo _ROOT_ . "/img/logo.png"; ?>" id='logo'/>
 	</a>
 	<div id="account">
 		<?php 
 		if ($_SESSION['log'])
-			include ('/template-parts/account_nav.php');
+			include (_ROOT_ . '/template-parts/account_nav.php');
 		else
-			include ('template-parts/login.php');
+			include (_ROOT_ . '/template-parts/login.php');
 		?>
 	</div>
 	<nav id="main_nav">
