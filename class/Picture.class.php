22185e351487c1347f1352fab6142c2cb6261583
wPef <wPef@MacBook-Pro-de-Foulques.local>
2017-02-09 23:35:47 +0100

    debugging picture class

--- class/Picture.class.php
+++ class/Picture.class.php
@@ -1,23 +1,24 @@
 <?php
-
-//include_once(ROOT . "config/inc.php");
+//debug
+//include_once('../config/inc.php');
 //define('DIR', ROOT . "photos/");
-session_start(); //debug 
+//end
 
 class Picture {
 	public		$owner;
 	public		$src;
 	public		$name;
-	public		$comment;
+	public		$comments;
 	public		$likes;
+	public		$error;
 	public static $verbose = false;
 
 /* ==> DEFAULT METHOD <== */
 	public function __construct($datas) {
-		if (is_array($datas) && array_key_exists('src', $datas))
-			$this->proceed_array($datas);
+		if (is_array($datas))
+			$this->proceedArray($datas);
 		else
-			$this->__destruct();
+			$this->error = "NO DATAS";
 	}
 
 	public function __destruct() {
@@ -28,13 +29,15 @@ class Picture {
 	public function __toString () {
 		return "IMG :" . $this->src . " from " . $this->owner . PHP_EOL;
 	}
+/* ==> GET & SET <== */
 
 /* ==> My Functions <== */
 
-	public function proceed_array($a) {
+	public function proceedArray($a) {
 		foreach ($a as $d => $v)
 		{
 			switch ($d) {
+			//set vars;
 				case 'src':
 					$this->src = $v;
 					break;
@@ -45,10 +48,58 @@ class Picture {
 					$this->owner = $v;
 					break;
 			}
+			//push file & set unset vars;
+			if (array_key_exists('data', $a))
+				$this->proceedDatas($a['data']);
 		}
 	}
 
-	public function proceed() {
+	public function proceedDatas($d)
+	{		
+		//proceed datas;
+		$d = str_replace(' ','+',$d); //JS to PHP decode
+		$d = substr($d,strpos($d,",")+1); //remove data:img/png ...
+		$datas = base64_decode($d); //decode string
+		
+		//Check & upload
+		if (_checkDatas($datas))
+		{
+			//upload
+			if ($this->_uploadImg($datas))
+			{
+				//set vars;
+				if (!isset($this->name))
+					$this->name = $this->_getNewName();
+				if (!isset($this->src))
+					$this->src = DIR . $this->name;
+				if (!isset($this->owner))
+					$this->owner = $_SESSION['user']->login;
+			}
+		}
+		else
+			$this->error .= "WRONG FORMAT";
+	}
+
+	private function _uploadImg($img)
+	{
+		if (file_put_contents($img_src, $img))
+			return TRUE;
+		else
+			$this->error = "An error occured uploading the file.";
+			return FALSE;	
+	}
+
+	private function _getNewName()
+	{
+		$i = 0;
+		do {
+			$file = 'photo' . $i . '.png';
+			$i++;
+			} while (file_exists(DIR . $file));
+		return ($file); 
+	}
+
+	private function _checkDatas($d) {
 		//proceed HERE;
 		return TRUE;
 	}
@@ -58,13 +109,11 @@ class Picture {
 	}
 }
 
-// $new = array(
-// 	'src' => "yyo",
-// 	'name' => "photo",
-// 	'owner' => $_SESSION['user']->login);
+$new = array( 'data' => file_get_contents(DIR . 'img64.png')
+	);
 
-// $pict = new Picture($new);
-// if (!empty($pict))
-// 	echo $pict;
+$pict = new Picture($new);
+if (!empty($pict))
+	echo $pict;
 
 ?>
--- mods/upload.php
+++ mods/upload.php
@@ -5,40 +5,27 @@ define('DIR', ROOT . "photos/");
 
 $imgb64 = $_POST["pic"];
 
-if (empty($_POST) || empty($imgb64) ) {
-	$_SESSION['alert'] = 'ERROR : No data received!';
-	exit();
-}
+ if (empty($_POST) || empty($imgb64) ) {
+ 	$_SESSION['alert'] = 'ERROR : No data received!';
+ 	exit();
+ }
 
-$imgb64 = str_replace(' ','+',$imgb64); //JS to PHP decode
-$imgb64 = substr($imgb64,strpos($imgb64,",")+1); //remove data:img/png ...
-$img = base64_decode($imgb64); //decode string
-$img_name = get_photo_name();
-$img_src = DIR . $img_name;
+// $imgb64 = str_replace(' ','+',$imgb64); //JS to PHP decode
+// $imgb64 = substr($imgb64,strpos($imgb64,",")+1); //remove data:img/png ...
+// $img = base64_decode($imgb64); //decode string
+// $img_name = get_photo_name();
+// $img_src = DIR . $img_name;
 
-// BUG but array is OK for sure;
-$new = array(
-	'src' => "$img_src",
-	'name' => "$img_name",
-	'owner' => $_SESSION['user']->login);
+// $new = array(
+// 	'src' => $img_src,
+// 	'name' => $img_name,
+// 	'owner' => $_SESSION['user']->login);
 
-if (!empty($pict = new Picture($new)))
-	$_SESSION['message'] = "$pict";
+ file_put_contents(DIR . 'img64.png', $imgb64);
 
+$pict = new Picture(array('data' => $imgb64)); //tested
 
-if (file_put_contents($img_src, $img))
-	$_SESSION['message'] = "Photo uploaded.";
-else
-	$_SESSION['alert'] .= "An error occured uploading the file.";
-
-function get_photo_name()
-{
-	$i = 0;
-	do {
-		$file = 'photo' . $i . '.png';
-		$i++;
-		} while (file_exists(DIR . $file));
-	return ($file); 
-}
+if (!empty($pict->error))
+	echo $pict->error;
 
 ?>
