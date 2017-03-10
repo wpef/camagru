b01f94fcf683ef19e0f2025b91a74732c92ba3f8
wPef <wPef@MacBook-Pro-de-Foulques.local>
2017-03-10 18:52:48 +0100

    styled edit account

--- template-parts/modif_account.php
+++ template-parts/modif_account.php
@@ -1,13 +1,17 @@
 <?php if(!defined('access')) { $_SESSION['alert'] = 'Direct access not permitted'; header('Location: ..?error=direct_access');} ?>
+<div class="form">
+	<h2>Edit account</h2>
 <form name='modif' action="<? echo WEBROOT . 'mods/modif_account.php'; ?>" method='post' />
 	Login (cannot be modify):
-	<input type='text' name='login' value='<?php echo $_SESSION['user']->login ?>' readonly/><br>
+	<input type='email' name='login' value='<?php echo $_SESSION['user']->login ?>' readonly/><br>
 	Mail :
 	<input type='text' name='mail' value='<?php echo $_SESSION['user']->mail ?>' /><br>
-	Password :<br>
+	<fieldset>
+	<legend>Password</legend>
 	Old password : <input type='password' name='old_pw'/><br>
 	New password : <input type='password' name='new_pw1'/><br>
 	Confirm new password : <input type="password" name='new_pw2'/><br>
+	</fieldset>
 	First name :
 	<input type='text' name='f_name' value='<?php echo $_SESSION['user']->f_name ?>' /><br>
 	Last Name :
@@ -15,3 +19,4 @@
 	<br>
 	<input type="submit" name="submit" value="Apply modifications">
 </form>
+</div>
