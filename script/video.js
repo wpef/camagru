ab7784b064b80e9dbc3e2d1e3c3f3cf4a2818593
fde-monc <fde-monc@student.42.fr>
2017-03-27 16:39:10 +0200

    js display OK // TODO Video css

--- script/video.js
+++ script/video.js
@@ -3,6 +3,8 @@
 //setting vars
 var		streaming	= false,
 		upload		= false,
+		body		= document.querySelector('.body'),
+		vDiv		= document.querySelector('#video-div'),
 		video       = document.querySelector('#video'),
 		cover       = document.querySelector('#cover'),
 		canvas      = document.querySelector('#canvas'),
@@ -20,17 +22,6 @@ if (width > 650)
 upload_form.style.display 	= 'none';
 startbutton.style.display   = 'none';
 sList.style.display = 'none';
-
-for (var i = 0; i < stickers.length; i++) {
-	stickers[i].addEventListener("click", function() {
-		if (stick_on)
-			cover.getContext('2d').clearRect(0, 0, cover.width, cover.height);
-		cover.getContext('2d').drawImage(this, 0, 0, cover.width, cover.height);
-		stick_on = true;
-		startbutton.style.display   = 'initial';
-	}, false);
-};
-
 
 navigator.getMedia = (	navigator.getUserMedia ||
 						navigator.webkitGetUserMedia ||
@@ -54,6 +45,7 @@ navigator.getMedia(
 
 function activeVideo () {
 	sList.style.display = 'block';
+	activeStickers();
 	video.addEventListener('canplay', function(ev) {
 		if (!streaming) {
 				height = video.videoHeight / (video.videoWidth/width);
@@ -76,21 +68,24 @@ function activeVideo () {
 function displayUploadForm() {
 	upload_form.style.display	= 'initial';
 	upload_form.querySelector("input").onchange = function (event) {
-		//check file
+		//INSERT check file here
 		var prev = document.createElement("img");
 			prev.src = URL.createObjectURL(event.target.files[0]);
 			prev.id = "upload_prev";
-		video.parentNode.replaceChild(prev, video);
-		event.target.parentNode.style.display = "none";
-		prev.onload = function () {
-			sList.style.display = 'block';
-			cover.setAttribute('width', prev.width);
-			cover.setAttribute('height', prev.height);
-			startbutton.addEventListener('click', function(ev) {
-				takepicture(prev);
-				ev.preventDefault();
-			}, false);
-		};
+			prev.onload = function () {
+				upload_form.style.display = 'none';
+				sList.style.display = 'block';
+				vDiv.replaceChild(prev, video);
+
+				cover.setAttribute('width', document.querySelector('#upload_prev').width);
+				cover.setAttribute('height', document.querySelector('#upload_prev').height);
+
+				activeStickers();
+				startbutton.addEventListener('click', function(ev) {
+					takepicture(prev);
+					ev.preventDefault();
+				}, false);
+			};
 	};
 }
 
@@ -134,6 +129,19 @@ function activeButtons () {
 		title.onfocus = function () {
 			this.className = "pic_name editing";
 		}
+	}
+}
+
+function activeStickers () {
+	for (var i = 0; i < stickers.length; i++) {
+		stickers[i].addEventListener("click", function() {
+			console.log('activ');
+			if (stick_on)
+				cover.getContext('2d').clearRect(0, 0, cover.width, cover.height);
+			cover.getContext('2d').drawImage(this, 0, 0, cover.width, cover.height);
+			stick_on = true;
+			startbutton.style.display   = 'initial';
+		}, false);
 	}
 }
 
--- template-parts/video.php
+++ template-parts/video.php
@@ -1,12 +1,12 @@
 <?php if(!defined('access')) { $_SESSION['alert'] = 'Direct access not permitted'; header('Location: ..?error=direct_access');} ?>
 <?php include(ROOT . 'template-parts/stickers_list.php') ?>
 <!-- VIDEO -->
-<canvas id="cover" style='position:absolute'></canvas>
-<video id="video"></video>
-<button id="startbutton">Prendre une photo</button>
-<canvas id="canvas" style="display: none"></canvas>
-<div id="preview"></div>
-
+<div id="video-div">
+	<video id="video"></video>
+	<canvas id="canvas" style="display: none"></canvas>
+	<canvas id="cover"></canvas>
+</div>
+<button id="startbutton">Take a picture</button>
 
 <!-- Upload -->
 <p id="uploadForm" align="center"><br/>
@@ -14,5 +14,8 @@
 		File: <input name="uploaded_file" type="file" size="30" />
 	</label>
 </p>
+
+<div id="preview"></div>
+
 <script type="text/javascript" src="<?php echo WEBROOT . 'script/video.js'?>"></script>
 <script type="text/javascript" src="<?php echo WEBROOT . 'script/grid_ui.js'?>"></script>
