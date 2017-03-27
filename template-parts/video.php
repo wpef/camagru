1da546887e484e8c0ce5b9602c794a9fb0fcccc8
fde-monc <fde-monc@student.42.fr>
2017-03-27 18:32:29 +0200

    new strucure js ok

--- template-parts/video.php
+++ template-parts/video.php
@@ -1,19 +1,21 @@
 <?php if(!defined('access')) { $_SESSION['alert'] = 'Direct access not permitted'; header('Location: ..?error=direct_access');} ?>
+<div id="main">
 <?php include(ROOT . 'template-parts/stickers_list.php') ?>
 <!-- VIDEO -->
-<div id="video-div">
-	<video id="video"></video>
-	<canvas id="canvas" style="display: none"></canvas>
-	<canvas id="cover"></canvas>
-</div>
-<button id="startbutton">Take a picture</button>
+	<div id="video-div">
+		<video id="video"></video>
+		<canvas id="canvas" style="display: none"></canvas>
+		<canvas id="cover"></canvas>
+	</div>
+	<button id="startbutton">Take a picture</button>
 
-<!-- Upload -->
-<p id="uploadForm" align="center"><br/>
-	<label>
-		File: <input name="uploaded_file" type="file" size="30" />
-	</label>
-</p>
+	<!-- Upload -->
+	<p id="uploadForm" align="center"><br/>
+		<label>
+			File: <input name="uploaded_file" type="file" size="30" />
+		</label>
+	</p>
+</div>
 
 <div id="preview"></div>
 
